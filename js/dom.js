302f2fe1ca264ced31585d938e5f07723582c305
TanzidAnan <tanzid.anan125@gmail.com>
2025-08-05 01:34:08 +0600

    dom function

--- js/dom.js
+++ js/dom.js
@@ -1,1 +1,11 @@
-console.log('Hello welcome to DOM')
+console.log('Hello welcome to DOM');
+console.log(document.body);
+
+
+const student ={
+    name:'ABUL',
+    ID:11,
+    study:function(time){
+        console.log(time,'study kortese')
+    }
+}
