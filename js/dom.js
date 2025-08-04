199f9e464a70bcb138869eddc9867e412fc1594d
TanzidAnan <tanzid.anan125@gmail.com>
2025-08-05 02:48:28 +0600

    add remove

--- js/dom.js
+++ js/dom.js
@@ -18,3 +18,20 @@ const student ={
         // console.log(time,'study kortese')
     }
 }
+
+    // নতুন আইটেম যোগ করার ফাংশন
+      function addItem() {
+        const ul = document.getElementById("myList");
+        const li = document.createElement("li");
+        li.textContent = "নতুন ফল";
+        ul.appendChild(li);
+      }
+
+      // শেষ আইটেম মুছে ফেলার ফাংশন
+      function removeItem() {
+        const ul = document.getElementById("myList");
+        const lastItem = ul.lastElementChild;
+        if (lastItem) {
+          ul.removeChild(lastItem);
+        }
+      }
