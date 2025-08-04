e779c7075d01d1d2d4bea40d933bab173f17a836
TanzidAnan <tanzid.anan125@gmail.com>
2025-08-05 02:27:28 +0600

    change name

--- js/dom.js
+++ js/dom.js
@@ -2,16 +2,19 @@ console.log('Hello welcome to DOM');
 console.log(document.body);
 
 const liCollection=document.getElementsByTagName('li');
-console.log(liCollection);
+// console.log(liCollection);
+
+const changeName =document.getElementById('fruits-title');
+changeName.innerText='fruits change Name js'
 
 for(const list of liCollection){
-    console.log(list.innerText)
+    // console.log(list.innerText)
 }
 
 const student ={
     name:'ABUL',
     ID:11,
     study:function(time){
-        console.log(time,'study kortese')
+        // console.log(time,'study kortese')
     }
 }
