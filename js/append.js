2f909e12d516007e8deee4846581f8f4bfb8bdf7
TanzidAnan <tanzid.anan125@gmail.com>
2025-08-06 01:22:12 +0600

    class id dake dete hove

--- js/append.js
+++ js/append.js
@@ -2,4 +2,16 @@ const placesList =document.getElementById('places-list');
 const li =document.createElement('li');
 li.innerText='borisal';
 
-placesList.appendChild(li)
+placesList.appendChild(li);
+
+// main content
+
+const mainSection =document.getElementById('main-content')
+
+
+const section =document.createElement('section');
+const h1 =document.createElement('h1');
+h1.innerText='My food List';
+section.appendChild(h1);
+
+mainSection.appendChild(section)
