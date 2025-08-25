360194621c9bbc9fb4afb9b03edea86435eefae5
TanzidAnan <tanzid.anan125@gmail.com>
2025-08-25 21:51:19 +0600

    append

--- js/append.js
+++ js/append.js
@@ -14,4 +14,13 @@ const h1 =document.createElement('h1');
 h1.innerText='My food List';
 section.appendChild(h1);
 
+const ul =document.createElement('ul');
+const li1 =document.createElement('li');
+li1.innerText='ami bath kabo';
+ul.appendChild(li1);
+const li2 =document.createElement('li')
+li2.innerText='biryani';
+ul.appendChild(li2)
+section.appendChild(ul)
+
 mainSection.appendChild(section)
